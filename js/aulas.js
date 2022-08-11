bda78082ce470670d2dd2f962def5198ccf82be6
Luciano <luciano.ivec@gmail.com>
2022-08-11 03:04:21 -0300

    acompanhamento das aulas

---
+++ js/aulas.js
@@ -0,0 +1,19 @@
+function hello(){
+    console.log("Hello world!");
+}
+function variaveis(){
+//declarando variaveis
+var abacate = 2;
+var melancia = 3;
+
+
+var totalFrutasSacola = abacate + melancia;
+
+console.log("TotalFrutasSacola: " + totalFrutasSacola);
+
+abacate = 5;
+totalFrutasSacola = abacate + melancia
+console.log("TotalFrutasSacola: " + totalFrutasSacola);
+return "Soma inicial e alteração da variavel";
+}
+    
